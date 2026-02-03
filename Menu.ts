104af3c415cc28a012e4311cc5f78be357d02acc
jrs-neto <jrd.santosneto@gmail.com>
2026-02-03 20:10:49 -0300

    Criação do Menu e inserção de cores

---
+++ Menu.ts
@@ -0,0 +1,74 @@
+import leia from "readline-sync";
+import { colors } from "./src/util/Colors";
+
+export function main() {
+  let opcao: number;
+
+  while (true) {
+    console.log(colors.bg.black, colors.fg.yellow, "*******************************************************");
+    console.log("                                                       ");
+    console.log("             BANCO DO BRAZIL COM Z                     ");
+    console.log("                                                       ");
+    console.log("*******************************************************");
+    console.log("                                                       ");
+    console.log("           1 - Criar Conta                             ");
+    console.log("           2 - Listar todas as Contas                  ");
+    console.log("           3 - Buscar Conta por Número                 ");
+    console.log("           4 - Atualizar Dados da Conta                ");
+    console.log("           5 - Apagar Conta                            ");
+    console.log("           6 - Sacar                                   ");
+    console.log("           7 - Depositar                               ");
+    console.log("           8 - Transferir valores entre contas         ");
+    console.log("           9 - Sair                                    ");
+    console.log("                                                       ");
+    console.log("*******************************************************");
+    console.log("                                                       ");
+
+    opcao = leia.questionInt("Entre com a opcao desejada: ");
+
+    if (opcao === 9) {
+      console.log(colors.fg.greenstrong, "\nBanco do Brazil com Z - O seu Futuro começa aqui!");
+      sobre();
+      process.exit(9);
+    }
+
+    switch (opcao) {
+      case 1:
+        console.log(colors.fg.whitestrong, "\n\nCriar conta\n\n", colors.reset);
+        break;
+      case 2:
+        console.log(colors.fg.whitestrong, "\n\nListar todas as contas\n\n", colors.reset);
+        break;
+      case 3:
+        console.log(colors.fg.whitestrong, "\n\nConsultar dados da Conta - por número\n\n", colors.reset);
+        break;
+      case 4:
+        console.log(colors.fg.whitestrong, "\n\nAtualizar dados da Conta\n\n", colors.reset);
+        break;
+      case 5:
+        console.log(colors.fg.whitestrong, "\n\nApagar uma Conta\n\n", colors.reset);
+        break;
+      case 6:
+        console.log(colors.fg.whitestrong, "\n\nSaque\n\n", colors.reset);
+        break;
+      case 7:
+        console.log(colors.fg.whitestrong, "\n\nDepósito\n\n", colors.reset);
+        break;
+      case 8:
+        console.log(colors.fg.whitestrong, "\n\nTransferência entre Contas\n\n", colors.reset);
+        break;
+      default:
+        console.log(colors.fg.whitestrong, "Operação Inválida!", colors.reset);
+    }
+  }
+}
+
+export function sobre(): void {
+  console.log("*******************************************************");
+  console.log("Programa desenvolvido por: José Rodrigues.");
+  console.log("Github:");
+  console.log("LinkedIn: ");
+  console.log("*******************************************************");
+}
+
+main();
